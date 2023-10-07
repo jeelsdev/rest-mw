1d9c8d6feb4d0735536d96381cb85dc4537a10a7
Joel Huaman <joelhuaman7m@gmail.com>
2023-10-07 00:45:53 -0500

    instalation of basic dependencies

---
+++ src/main/java/com/mw/machanicalworkshop/MachanicalWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mw.machanicalworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachanicalWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachanicalWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mw/machanicalworkshop/MachanicalWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mw.machanicalworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachanicalWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
